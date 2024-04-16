1d85b389093251341c24cc1afe5f92536eac4a0d
Luiz <l.guilherme128@gmail.com>
2024-04-16 06:29:17 -0300

    First Commit

---
+++ Material_list/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Material_list.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
